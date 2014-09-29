bcc020cc3a96006e072ac7d1e4349d6bbf7c2813
admin <admin@iMac-8.local>
2014-09-29 10:11:15 +0200

    Initial Commit

---
+++ FormationM2i/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FormationM2i
+//
+//  Created by admin on 29/09/2014.
+//  Copyright (c) 2014 fr.proxiad.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FormationM2i/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FormationM2i
+//
+//  Created by admin on 29/09/2014.
+//  Copyright (c) 2014 fr.proxiad.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
