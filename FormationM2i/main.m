6f974517798a3fed6c9319191c18fb44441bfe22
admin <admin@iMac-8.local>
2014-09-29 11:37:49 +0200

    AFN: J1 matin

--- FormationM2i/main.m
+++ FormationM2i/main.m
@@ -12,7 +12,53 @@
 
 int main(int argc, char * argv[])
 {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+//    @autoreleasepool {
+//        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+//    }
+    
+    int nombreA = 1;
+    int nombreB = 3;
+    
+    int total = nombreA+nombreB;
+    int monTab[] = {1,2,3};
+    
+    printf("Mon total est de %d \n",total);
+    
+    while(nombreA <= 10){
+        nombreA++;
+        printf("Mon nombreA incrémenté est de %d \n",nombreA);
+    }
+    
+    for(;nombreB<=10;nombreB++){
+        printf("Mon nombreB incrémenté est de %d \n",nombreB);
+    }
+    
+    BOOL isFormation=NO;
+    for(int i = 0;i < sizeof(monTab)/ sizeof(monTab[0]);i++){
+        
+        int var = monTab[i];
+        
+        if(var == 2 || isFormation){
+            printf("M%di Formation\n",var);
+            isFormation = YES;
+        } else {
+            printf("Mon tab a pour valeur %d \n",var);
+        }
+        
+        switch (var) {
+            case 1:
+                printf("Le 1 c'est cool \n");
+                break;
+            case 2:
+                printf("Le 2 c'est bien \n");
+                break;
+            case 3:
+                printf("Le 3 c'est moyen \n");
+                break;
+                
+            default:
+                break;
+        }
+        
     }
 }
